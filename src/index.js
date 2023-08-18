5015cc452b6df972e1ee882e606bab646b1f546f
RIPON KHAN <riponakondo4521@gmail.com>
2023-08-18 21:39:58 +0600

    added maximum work ok

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ function calculateTraingleArea() {
     // traingle calculete 
     const base = getInputValue('traingle-base');
     const height = getInputValue('traingle-heigt');
+    if (isNaN(base) || isNaN (height)) {
+        alert('Please insert a number!');
+        return;
+    }
     const area = 0.5 * base*height;
-    setElementText('traingle-area', area)
+    setElementText('traingle-area', area);
+    addToCalculationEntry('Trainagle', area);
     
 
     
@@ -18,9 +23,13 @@ function calculateTraingleArea() {
 function calculateRectangleArea() {
     const width = getInputValue('rechtengle-width');
     const lenthgh = getInputValue ('rectengle-lenthgh');
+    if (isNaN(width)|| isNaN(lenthgh)) {
+        alert('Please inseart number!');
+        return;
+    }
     const area = width * lenthgh ;
     setElementText('rectangle-area', area);
-    
+    addToCalculationEntry('Reactangle' , area)
     
     
 }
@@ -30,8 +39,13 @@ function calculateRectangleArea() {
 function calculateParallelogramArea() {
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height');
+    if (isNaN(base) || isNaN(height)) {
+        alert('Please inseart  Number!');
+        return;
+    }
     const area = base*height;
     setElementText('parallelogram-area', area);
+    addToCalculationEntry('Pralalaogram' , area)
 
     
 }
@@ -62,6 +76,7 @@ document.getElementById('btn-clear').addEventListener('click',function(){
     height.value = '';
     const area = document.getElementById('parallelogram-area');
     area.innerText = 0;
+ 
 
     // reactangle value clear
      const width = document.getElementById('rechtengle-width');
@@ -80,3 +95,19 @@ document.getElementById('btn-clear').addEventListener('click',function(){
     traingleArea.innerText = 0;
 
 })
+
+
+
+function addToCalculationEntry(areatype , area) {
+ 
+    
+    const calculationEntry = document.getElementById('calculation-entry');
+    const count = calculationEntry.childElementCount;
+    const p = document.createElement('p');
+    p.classList.add('my-5')
+    p.innerHTML =` ${count + 1}. ${areatype} ${area}
+    cm<sup>2</sup> <button class = " btn btn-success btn-sm">convert</button>
+
+    ` 
+    calculationEntry.appendChild(p);
+}
